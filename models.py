d2c378090f1dfc94e0e2a9b51a62cf271874ecf8
Hariz Munawar <munawarhariz@gmail.com>
2022-06-28 11:05:22 +0700

    feat: add wrapper for shopee product detail api

---
+++ models.py
@@ -0,0 +1,4 @@
+from pydantic import BaseModel
+
+class ProductURL(BaseModel):
+    url: str
--- server.py
+++ server.py
@@ -1,8 +1,7 @@
-from email import charset
-from itertools import product
-import string
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
+from models import ProductURL
+from scraper.product import Product
 from shopee_scraper import *
 
 app = FastAPI()
@@ -12,7 +11,6 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-
 @app.get("/items/{username}")
 async def read_items(username: str, q: Union[str, None] = None):
     shopee_scrapper = ShopeeScrapper(username)
@@ -21,3 +19,17 @@ async def read_items(username: str, q: Union[str, None] = None):
     if not error_msg: data['data'] = shopee_scrapper.generate_items_json(products)
     data['message'] = error_msg
     return data
+
+@app.get("/products/")
+def get_product(payload: ProductURL, response: Response):
+    product = Product(payload.url)
+    data = product.info().serialize()
+
+    if data:
+        response.status_code = 200
+        response.body = data
+    else:
+        response.status_code = 500
+        response.body = {'error': 'Something went wrong'}
+    
+    return response.body
